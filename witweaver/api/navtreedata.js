95a0fa814a9f39fc378383f13447456a44ba22df
Ornlu2 <Ornlu2@users.noreply.github.com>
2026-09-03 03:31:39 +0000

    deploy: 4b5108208ba55567717760c74bb0dbf86ab942c7

--- witweaver/api/navtreedata.js
+++ witweaver/api/navtreedata.js
@@ -24,7 +24,7 @@
 */
 var NAVTREE =
 [
-  [ "WitWeaver", "index.html", [
+  [ "WitWeaver™", "index.html", [
     [ "Namespaces", "namespaces.html", [
       [ "Namespace List", "namespaces.html", "namespaces_dup" ],
       [ "Namespace Members", "namespacemembers.html", [
